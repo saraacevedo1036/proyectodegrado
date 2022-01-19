3a2ea980d01e7b286b32ce07df4a7ee0e7fdf2f6
CEIBA\alejandro.jaramillo <alejandro.jaramillo@ceiba.com.co>
2022-01-18 22:02:07 -0500

    Se corrigen errores

--- src/main/java/co/proyectoGrado/proyectoGrado/web/security/SecurityConfig.java
+++ src/main/java/co/proyectoGrado/proyectoGrado/web/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package co.proyectoGrado.proyectoGrado.web.security;
 
-import com.platzi.market.domain.service.PlatziUserDetailsService;
-import com.platzi.market.web.security.filter.JwtFilterRequest;
+import co.proyectoGrado.proyectoGrado.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,20 +9,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private PlatziUserDetailsService platziUserDetailsService;
+    private UserDetailsService userDetailsService;
 
     @Autowired
     private JwtFilterRequest jwtFilterRequest;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(platziUserDetailsService);
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
